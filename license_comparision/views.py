692a5227ded0b034cd79188c504f99cd4364dd80
Worlako Dzokoto <worlaco.dzo@gmail.com>
2022-11-22 06:23:33 +0000

    Updated the CRUD api for license comparision

--- license_comparision/views.py
+++ license_comparision/views.py
@@ -22,6 +22,7 @@ from utils.dowell import (
 
 
 from .serializers import ComparisionSerializer
+from licenses.views import SoftwareLicenseList
 
 
 class ComparisionList(APIView):
@@ -37,23 +38,19 @@ class ComparisionList(APIView):
             response_json = {}
             status_code = 500
 
-            if action_type == "search":
-                response_json, status_code = self.search_license(
-                    request, format)
-            else:
-                # Retrieve license on remote server
-                response_json = fetch_document(
-                    collection=SOFTWARE_LICENSE_COLLECTION,
-                    document=SOFTWARE_LICENSE_DOCUMENT_NAME,
-                    fields={}
-                )
+            # Retrieve comparision
+            response_json = fetch_document(
+                collection=ATTRIBUTE_COLLECTION,
+                document=ATTRIBUTE_DOCUMENT_NAME,
+                fields={"attributes.attribute_type": "comparisions"}
+            )
 
-                response_json = self.add_license_logo_url(response_json)
-                status_code = status.HTTP_200_OK
+            status_code = status.HTTP_200_OK
 
             return Response(
                 response_json,
                 status=status_code)
+
 
         # The code below will
         # execute when error occur
@@ -70,34 +67,50 @@ class ComparisionList(APIView):
             response_json = {}
             status_code = 500
 
-            from datetime import date
+
             request_data = request.data
 
-            action_type = ""
-            if "action_type" in request_data:
-                action_type = request_data["action_type"]
+            license_1_event_id = request_data['license_1_event_id']
+            license_2_event_id = request_data['license_2_event_id']
 
-            if action_type == "check-compatibility":
-                response_json, status_code = self.check_license_compatibility(
-                    request, format)
-            else:
-                request_data["is_active"] = True
+            
+            license_1 = self.get_license(license_1_event_id)["data"][0]["softwarelicense"]
+            license_2 = self.get_license(license_2_event_id)["data"][0]["softwarelicense"]
 
-                # Convert release date string (yyyy-mm-dd)
-                # to date object
-                request_data["released_date"] = date.fromisoformat(
-                    request_data["released_date"])
+            license_1_logo_url = ""
+            if "logo_detail" in license_1:
+                license_1_logo_url = license_1["logo_detail"]["url"]
 
-                serializer = SoftwareLicenseSerializer(data=request_data)
+            license_2_logo_url = ""
+            if "logo_detail" in license_1:
+                license_2_logo_url = license_2["logo_detail"]["url"]
 
-                # Commit data to database
-                serializer.is_valid()
-                response_json, status_code = serializer.save()
+
+
+            request_data['attribute_type'] = "comparisions"
+            request_data['indentifier'] = f"{license_1_event_id}+{license_2_event_id},{license_2_event_id}+{license_1_event_id}"
+            request_data['license_1_logo_url'] = license_1_logo_url
+            request_data['license_2_logo_url'] = license_2_logo_url
+            request_data['license_1_name'] = license_1["license_name"]
+            request_data['license_2_name'] = license_2["license_name"]
+            request_data['license_1_version'] = license_1["version"]
+            request_data['license_2_version'] = license_2["version"]
+            request_data['license_1_event_id'] = license_1_event_id
+            request_data['license_2_event_id'] = license_2_event_id
+            request_data['comparisions'] = []
+
+            serializer = ComparisionSerializer(data=request_data)
+
+            # Commit data to database
+            print("working")
+            serializer.is_valid()
+            response_json, status_code = serializer.save()
 
             return Response(response_json,
                             status=status_code
                             )
 
+        
         # The code below will
         # execute when error occur
         except Exception as e:
@@ -108,121 +121,22 @@ class ComparisionList(APIView):
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
-    # CHECK FOR COMPATIBILITY HERE
-
-    def check_license_compatibility(self, request, format=None):
-        """ Check for two licnese and return True if 
-            license_one == license_two
-        """
-        is_compatible = False
-        percentage_of_comaptibility = 0
-        try:
-
-            license_event_id_one = request.data.get("license_event_id_one", "")
-            license_event_id_two = request.data.get("license_event_id_two", "")
-
-            # Retrieve license on remote server
-            # Get license two
-            license_one_json = fetch_document(
-                collection=SOFTWARE_LICENSE_COLLECTION,
-                document=SOFTWARE_LICENSE_DOCUMENT_NAME,
-                fields={"eventId": license_event_id_one}
-            )
-            license_one = license_one_json["data"][0]['softwarelicense']
-
-            # Get license two
-            license_two_json = fetch_document(
-                collection=SOFTWARE_LICENSE_COLLECTION,
-                document=SOFTWARE_LICENSE_DOCUMENT_NAME,
-                fields={"eventId": license_event_id_two}
-            )
-
-            # Get license compatible list
-            license_two = license_two_json["data"][0]['softwarelicense']
-            license_compatibility = license_two["license_compatibility"]
-
-            # Check for compatibility
-            for compatible in license_compatibility:
-                if license_one["license_name"]\
-                    == compatible['license']\
-                        and compatible["is_compatible"]:
-
-                    is_compatible = compatible["is_compatible"]
-                    percentage_of_comaptibility = compatible["percentage_of_comaptibility"]
-                    is_compatible = compatible["is_compatible"]
-
-            return ({
-                "is_compatible": is_compatible,
-                "percentage_of_comaptibility": percentage_of_comaptibility,
-                "disclaimer": license_two["disclaimer"],
-                "recommendation": license_two["recommendation"],
-                "license_one": license_one["license_name"],
-                "license_two": license_two["license_name"]
-
-            }), status.HTTP_200_OK
-
-        # The code below will
-        # execute when error occur
-        except Exception as e:
-            print(f"{e}")
-            return {"error_msg": f"{e}"}, status.HTTP_500_INTERNAL_SERVER_ERROR
-
-    # SEARCH FOR LICENSES HERE
-
-    def search_license(self, request, format=None):
-        """ Load linceses base on search term
-        """
-        try:
-            print("I was called")
-            limit = RECORD_PER_PAGE
-            offset = int(request.GET.get("offset", "0"))
-            search_term = request.GET.get("search_term", "")
-            response_json = {}
-
-            # Retrieve license on remote server
-            response_json = fetch_document(
-                collection=SOFTWARE_LICENSE_COLLECTION,
-                document=SOFTWARE_LICENSE_DOCUMENT_NAME,
-                fields={"softwarelicense.license_name": {
-                    "$regex": f"{search_term}", "$options": "i"}}
-            )
-
-            return response_json, status.HTTP_200_OK
-
-        # The code below will
-        # execute when error occur
-        except Exception as e:
-            print(f"{e}")
-            return {"error_msg": f"{e}"}, status.HTTP_500_INTERNAL_SERVER_ERROR
-
     @staticmethod
-    def add_license_logo_url(response_data):
-        data_list = response_data['data']
-        new_data_list = []
+    def get_license(event_id):
+        
+        # Retrieve license
+        response_json = fetch_document(
+            collection=SOFTWARE_LICENSE_COLLECTION,
+            document=SOFTWARE_LICENSE_DOCUMENT_NAME,
+            fields={"eventId": event_id}
+        )
 
-        for data in data_list:
-            softwarelicense = data['softwarelicense']
-            # this code only execute
-            # if the license object
-            # does not have logo_detail field
-            if "logo_detail" not in softwarelicense:
-                new_data_list.append(data)
-                continue
+        response_json = SoftwareLicenseList.add_license_logo_url(
+            response_json)
 
-            logo_detail = softwarelicense['logo_detail']
-            logo_detail['url'] = f'{BASE_IMAGE_URL}{logo_detail["filename"]}'
 
-            # update license logo detail
-            softwarelicense['logo_detail'] = logo_detail
+        return response_json
 
-            # add licence to new data list
-            data['softwarelicense'] = softwarelicense
-            new_data_list.append(data)
-
-        if new_data_list:
-            response_data['data'] = new_data_list
-
-        return response_data
 
 
 class ComparisionDetail(APIView):
@@ -232,21 +146,13 @@ class ComparisionDetail(APIView):
 
     def get(self, request, event_id, format=None):
         try:
-            # # Localhost
-            # license = SoftwareLicense.objects.get(license_id = license_id)
-            # # Serialize data
-            # serializer = SoftwareLicenseSerializer()
-            # data = serializer.to_representation(license.document, license.id)
 
-            # Retrieve license on remote server
+            # Get comparision
             response_json = fetch_document(
-                collection=SOFTWARE_LICENSE_COLLECTION,
-                document=SOFTWARE_LICENSE_DOCUMENT_NAME,
-                fields={"eventId": event_id}
+                collection=ATTRIBUTE_COLLECTION,
+                document=ATTRIBUTE_DOCUMENT_NAME,
+                fields={"eventId": event_id, "attributes.attribute_type": "comparisions"}
             )
-
-            response_json = SoftwareLicenseList.add_license_logo_url(
-                response_json)
             return Response(response_json, status=status.HTTP_200_OK)
 
         # The code below will
@@ -261,31 +167,85 @@ class ComparisionDetail(APIView):
 
     def put(self, request, event_id, format=None):
         try:
-            from datetime import date
+
             request_data = request.data
+            comparision = request_data["comparision"]
 
-            # Convert release date string (yyyy-mm-dd)
-            # to date object
-            request_data["released_date"] = date.fromisoformat(
-                request_data["released_date"])
+            action_type = ""
+            if "action_type" in request_data:
+                action_type = request_data["action_type"]
 
-            # Update and Commit data into database
-            serializer = SoftwareLicenseSerializer(
-                event_id, data=request_data)
-            if serializer.is_valid():
-                response_json, status_code = serializer.update(
-                    event_id, serializer.validated_data)
+            # Get license comparision 
+            response_json = fetch_document(
+                collection=ATTRIBUTE_COLLECTION,
+                document=ATTRIBUTE_DOCUMENT_NAME,
+                fields={"eventId": event_id}
+            )
+            license_comparison = response_json["data"][0]["attributes"]
 
-                return Response(
-                    response_json,
-                    status=status_code
-                )
 
-            else:
-                return Response({"error_msg": f"{serializer.errors}"},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                                )
+            if action_type == "add-license-category-comparison":
 
+                comparision['_id'] = str(uuid.uuid4())
+                license_comparison["comparisions"].append(comparision)
+
+
+                # Update
+                serializer = ComparisionSerializer(
+                    event_id, data=license_comparison)
+                if serializer.is_valid():
+                    response_json, status_code = serializer.update(
+                        event_id, serializer.validated_data)
+
+                    return Response(
+                        response_json,
+                        status=status_code
+                    )
+
+                else:
+                    return Response({"error_msg": f"{serializer.errors}"},
+                                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                                    )
+                
+            elif action_type == "update-license-category-comparison":
+
+                temp_comparisions = []
+                comparision_id = request_data["comparision_id"]
+
+                for data in license_comparison["comparisions"]:
+
+                    if comparision_id == data["_id"]:
+
+                        # Update license comparison object
+                        comparision_new = {**data, **comparision}
+                        temp_comparisions.append(comparision_new)
+
+                    else:
+                        temp_comparisions.append(data)
+
+                # Update list of license comparison object
+                license_comparison["comparisions"] = temp_comparisions
+
+
+                # Update
+                serializer = ComparisionSerializer(
+                    event_id, data=license_comparison)
+                if serializer.is_valid():
+                    response_json, status_code = serializer.update(
+                        event_id, serializer.validated_data)
+
+                    return Response(
+                        response_json,
+                        status=status_code
+                    )
+
+                else:
+                    return Response({"error_msg": f"{serializer.errors}"},
+                                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                                    )
+
+
+            
         # The code below will
         # execute when error occur
         except Exception as e:
